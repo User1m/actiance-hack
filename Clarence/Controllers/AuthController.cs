65210183316fb652fe1360ab0332e15a1b6d6cf1
Claudius Mbemba <mbembac@gmail.com>
2017-08-15 11:10:49 -0700

    removed owin nugets

--- Clarence/Controllers/AuthController.cs
+++ Clarence/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Routing;
 
 // The following using statements were added for this sample.
 
@@ -11,7 +12,7 @@ using System.Web.Http;
 
 namespace Actiance.Controllers
 {
-    public class AccountController : ApiController
+    public class AuthController : ApiController
     {
 
         private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
@@ -20,12 +21,18 @@ namespace Actiance.Controllers
         private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
         //private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
 
-        string authority = string.Format("https://{0}/{1}/adminconsent?client_id={3}&state=auth&redirect_uri={4}", aadInstance, tenant, clientId, redirectUri);
+        static string authority = string.Format("https://{0}/{1}/adminconsent?client_id={3}&state=auth&redirect_uri={4}", aadInstance, tenant, clientId, redirectUri);
         //eg. "https://login.microsoftonline.com/actiancehack.onmicrosoft.com/adminconsent?client_id=e5b5c8c1-2b25-4437-ba24-98d665a10f05&state=12345&redirect_uri=https://5b59e015.ngrok.io"
 
-        public void AdminConsent()
+        public static void AdminConsent()
         {
             System.Diagnostics.Process.Start(authority);
+        }
+
+        [ActionName("DefaultAction")]
+        public string Get()
+        {
+            return "hi";
         }
 
         public string PostForOauthToken()
--- Clarence/Startup.cs
+++ Clarence/Startup.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using Owin;
-using Microsoft.Owin;
-using System.Threading.Tasks;
-using Owin;
-using Microsoft.Owin.Security;
-using System.Configuration;
-using System.Globalization;
-using System.Threading.Tasks;
-using Microsoft.Owin.Security.ActiveDirectory;
-using Microsoft.Owin.Security.Cookies;
+﻿//using System;
+//using Owin;
+//using Microsoft.Owin;
+//using System.Threading.Tasks;
+//using Owin;
+//using Microsoft.Owin.Security;
+//using System.Configuration;
+//using System.Globalization;
+//using System.Threading.Tasks;
+//using Microsoft.Owin.Security.ActiveDirectory;
+//using Microsoft.Owin.Security.Cookies;
+//using Actiance.Controllers;
 
-//[assembly: OwinStartup(typeof(Actiance.Startup))]
+////[assembly: OwinStartup(typeof(Actiance.Startup))]
 
-namespace Actiance
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            //app.UseWelcomePage();
-            //System.Diagnostics.Process.Start("https://login.microsoftonline.com/actiancehack.onmicrosoft.com/adminconsent?client_id=3b3a45c4-b594-4a53-a71c-f34440d50f48&state=12345&redirect_uri=https://5b59e015.ngrok.io/");
-            //ConfigureAuth(app);
-        }
+//namespace Actiance
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            //app.UseWelcomePage();
+//            //ConfigureAuth(app);
+//        }
 
-        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
-        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
-        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
-        //private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
-        //private static string authority = ConfigurationManager.AppSettings["ida:Authority"];
-        private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
+//        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+//        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+//        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+//        //private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+//        //private static string authority = ConfigurationManager.AppSettings["ida:Authority"];
+//        private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
 
-        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+//        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
 
-        public void ConfigureAuth(IAppBuilder app)
-        {
-            Console.WriteLine(authority);
+//        public void ConfigureAuth(IAppBuilder app)
+//        {
+//            Console.WriteLine(authority);
 
-            // TODO: Hook up OWIN to perform OpenID Connect Authentication
+//            // TODO: Hook up OWIN to perform OpenID Connect Authentication
 
-            //app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+//            //app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
-            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+//            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
-            app.UseActiveDirectoryFederationServicesBearerAuthentication(
-                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
-                {
-                    Audience = ConfigurationManager.AppSettings["ida:Audience"],
-                    MetadataEndpoint = metadata
-                }
-            );
+//            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+//                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+//                {
+//                    Audience = ConfigurationManager.AppSettings["ida:Audience"],
+//                    MetadataEndpoint = metadata
+//                }
+//            );
 
-            //app.UseOpenIdConnectAuthentication(
-            //new OpenIdConnectAuthenticationOptions
-            //{
-            //    ClientId = clientId,
-            //    Authority = authority,
-            //    PostLogoutRedirectUri = postLogoutRedirectUri,
-            //    Notifications = new OpenIdConnectAuthenticationNotifications
-            //    {
-            //        AuthenticationFailed = context =>
-            //        {
-            //            context.HandleResponse();
-            //            context.Response.Redirect("/Error?message=" + context.Exception.Message);
-            //            return Task.FromResult(0);
-            //        }
-            //    }
-            //});
-        }
-    }
-}
+//            //app.UseOpenIdConnectAuthentication(
+//            //new OpenIdConnectAuthenticationOptions
+//            //{
+//            //    ClientId = clientId,
+//            //    Authority = authority,
+//            //    PostLogoutRedirectUri = postLogoutRedirectUri,
+//            //    Notifications = new OpenIdConnectAuthenticationNotifications
+//            //    {
+//            //        AuthenticationFailed = context =>
+//            //        {
+//            //            context.HandleResponse();
+//            //            context.Response.Redirect("/Error?message=" + context.Exception.Message);
+//            //            return Task.FromResult(0);
+//            //        }
+//            //    }
+//            //});
+//        }
+//    }
+//}
