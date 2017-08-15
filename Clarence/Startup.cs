51f242fb2a5492e6f2f1ace31342bbcdb2027db0
Claudius Mbemba <mbembac@gmail.com>
2017-08-14 20:17:46 -0700

    working on oauth

---
+++ Clarence/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Actiance.Startup))]
+
+namespace Actiance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
